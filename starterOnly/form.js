22fad1788bfa5c161d3cdb3b4b62db43592de995
Noëmie <ncav@protonmail.com>
2025-02-22 22:03:31 +0100

    refactor(form): improve form validation and fix HTML errors
    - Refactors form validation code for better readability and maintainability
    - Fixes text mistake in the HTML
    - Adds missing
    ame attribute to the checkbox

--- starterOnly/form.js
+++ starterOnly/form.js
@@ -1,5 +1,3 @@
-
-// Sélection des éléments du formulaire
 const formElement = document.querySelector("form[name='reserve']");
 const firstNameElement = document.getElementById("first");
 const lastNameElement = document.getElementById("last");
@@ -9,119 +7,72 @@ const quantityElement = document.getElementById("quantity");
 const locationElements = document.querySelectorAll("input[name='location']");
 const checkboxTermsElement = document.getElementById("checkbox1");
 const submitButton = document.querySelector(".btn-submit");
+const formElements = document.querySelectorAll("form[name='reserve'] input");
+
+const validationRules = {
+    first: {
+        regex: /^[A-Za-zÀ-ÖØ-öø-ÿ-]{2,}$/,
+        error: "Veuillez entrer 2 caractères ou plus pour le champ du prénom."
+    },
+    last: {
+        regex: /^[A-Za-zÀ-ÖØ-öø-ÿ-]{2,}$/,
+        error: "Veuillez entrer 2 caractères ou plus pour le champ du nom."
+    },
+    email: {
+        regex: /^[\w-]+(?:\.[\w-]+)*@(?:[\w-]+\.)*\w[\w-]{0,66}\.[a-z]{2,6}$/i,
+        error: "Veuillez indiquer un email valide"
+    },
+    birthdate: {
+        regex: /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/(\d{4})$/,
+        error: "Vous devez entrer votre date de naissance."
+    },
+    quantity: {
+        regex: /^[0-9]+$/,
+        error: "Veuillez entrer un nombre valide."
+    },
+    location: {
+        customValidation: () => Array.from(locationElements).some(el => el.checked),
+        error: "Vous devez choisir une option."
+    },
+    checkbox1: {
+        customValidation: () => checkboxTermsElement.checked,
+        error: "Vous devez accepter les termes et conditions."
+    }
+};
 
 function handleValidate(event){
    event.preventDefault();
 
-   // FIRST NAME
-   const regexName = /^[A-Za-zÀ-ÖØ-öø-ÿ-]{2,}$/
-   const testfirstname = regexName.test(firstNameElement.value)
-   if(testfirstname){
-      console.log("OK")
-      const parent = firstNameElement.parentNode
-      parent.setAttribute("data-error-visible", false);
-      parent.setAttribute("data-error", "");
+   formElements.forEach(input => {
+      console.log("input", input)
 
-   }else{
-      console.log("PAS OK")
-      const parent = firstNameElement.parentNode
-      parent.setAttribute("data-error-visible", true);
-      parent.setAttribute("data-error", "blabla");
-   }
-
-
-   // LAST NAME
-   const testlastname = regexName.test(lastNameElement.value)
-   if(testlastname){
-      console.log("OK")
-      const parent = lastNameElement.parentNode
-      parent.setAttribute("data-error-visible", false);
-      parent.setAttribute("data-error", "");
-   }else{
-      console.log("PAS OK")
-      const parent = lastNameElement.parentNode
-      parent.setAttribute("data-error-visible", true);
-      parent.setAttribute("data-error", "blabla");
-   }
-
-
-   // EMAIL
-   const regexEmail = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i
-   const testemail = regexEmail.test(emailElement.value)
-   if(testemail){
-      console.log("OK")
-      const parent = emailElement.parentNode
-      parent.setAttribute("data-error-visible", false);
-      parent.setAttribute("data-error", "");
-   }else{
-      console.log("PAS OK")
-      const parent = emailElement.parentNode
-      parent.setAttribute("data-error-visible", true);
-      parent.setAttribute("data-error", "blabla");
-   }
-
-
-  //  DATE
-   const birthdateRegex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/(\d{4})$/
-   const testbirthday = birthdateRegex.test(birthdateElement.value)
-   if(testbirthday){
-      console.log("OK")
-      const parent = birthdateElement.parentNode
-      parent.setAttribute("data-error-visible", false);
-      parent.setAttribute("data-error", "");
-   }else{
-      console.log("PAS OK")
-      const parent = birthdateElement.parentNode
-      parent.setAttribute("data-error-visible", true);
-      parent.setAttribute("data-error", "blabla");
-   }
-
-
-   // QUANTITY
-   const quantityRegex = /^[0-9]+$/
-   const testquantity = quantityRegex.test(quantityElement.value)
-   if(testquantity){
-      console.log("OK")
-      const parent = quantityElement.parentNode
-      parent.setAttribute("data-error-visible", false);
-      parent.setAttribute("data-error", "");
-   }else{
-      console.log("PAS OK")
-      const parent = quantityElement.parentNode
-      parent.setAttribute("data-error-visible", true);
-      parent.setAttribute("data-error", "blabla");
-   }
-
-
-   checkRadio()
-   function checkRadio() {
-      // Vérifie si au moins un bouton radio avec le name="option" est coché
-      let isChecked = document.querySelector('input[name="location"]:checked') !== null;
-
-      if (isChecked){
-         console.log("c'est OK")
-         const parent = document.querySelector('input[name="location"]').parentNode
-         parent.setAttribute("data-error-visible", false);
-         parent.setAttribute("data-error", "");
-      }else {
-         console.log("C'est PAS OK")
-         const parent = document.querySelector('input[name="location"]').parentNode
-         parent.setAttribute("data-error-visible", true);
-         parent.setAttribute("data-error", "blabla");
+      function showValidationError(input, isValid, errorMessage = "") {
+         const parent = input.parentNode;
+         parent.setAttribute("data-error-visible", !isValid);
+         parent.setAttribute("data-error", isValid ? "" : errorMessage);
       }
-   }
+      
+      const rule = validationRules[input.name];
+      if(rule){
+         const isValid = rule.customValidation ? rule.customValidation() : rule.regex.test(input.value);
+         showValidationError(input, isValid, rule.error)
+      }
+   })
 
-   if(checkboxTermsElement.checked){
-      console.log("OK")
-      const parent = checkboxTermsElement.parentNode
-      parent.setAttribute("data-error-visible", false);
-      parent.setAttribute("data-error", "");
-   }else{
-      console.log("PAS OK")
-      const parent = checkboxTermsElement.parentNode
-      parent.setAttribute("data-error-visible", true);
-      parent.setAttribute("data-error", "blabla");
-   }
+//   //  DATE
+//    const birthdateRegex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/(\d{4})$/
+//    const testbirthday = birthdateRegex.test(birthdateElement.value)
+//    if(testbirthday){
+//       console.log("OK")
+//       const parent = birthdateElement.parentNode
+//       parent.setAttribute("data-error-visible", false);
+//       parent.setAttribute("data-error", "");
+//    }else{
+//       console.log("PAS OK")
+//       const parent = birthdateElement.parentNode
+//       parent.setAttribute("data-error-visible", true);
+//       parent.setAttribute("data-error", "blabla");
+//    }
 
 }
 
